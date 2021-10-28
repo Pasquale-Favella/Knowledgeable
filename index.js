d339dda7beab1c903f0d286d34ffd3f787ccde2e
Favella Pasquale <FavellaPa@valueteam.com>
2021-10-28 18:00:57 +0200

    removed proxy local development from client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 8000;
 
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const Static = require('./middlewares/static.middleware');
 const SearchRoute = require('./routes/wikisearch.route');
@@ -9,6 +10,8 @@ const SearchRoute = require('./routes/wikisearch.route');
 const app = express();
 app.use(express.json());
 app.use(express.static('client/build'));
+
+if(process.env.NODE_ENV !== 'production') app.use(cors())
 
 
 app.use('/api/search', SearchRoute );
